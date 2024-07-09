826f57d8945b426cc1f1770d09ba9e115bb1e2e8
Michael Cervantes <indiedevmichael@gmail.com>
2024-07-09 10:08:23 -0700

    Added AssignmentsStudentView.js, CourseEnroll.js, ScheduleView.js

--- src/components/student/AssignmentsStudentView.js
+++ src/components/student/AssignmentsStudentView.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {SERVER_URL} from '../../Constants';
 
 // student views a list of assignments and assignment grades 
 // use the URL  /assignments?studentId= &year= &semester=
@@ -9,10 +10,91 @@ import React from 'react';
 
 const AssignmentsStudentView = (props) => {
     
+    const studentId = 3;
+    const headers = ['Course ID', 'Assignment', 'Due Date', 'Score'];
+    const [assignments, setAssignment] = useState ([
+        //Test data
+        // {courseId: 'cst338', title: 'Markov', dueDate: '2024-7-8', score: 99},
+        // {courseId: 'cst338', title: 'RPG Game', dueDate: '2024-7-8', score: 70},
+        // {courseId: 'cst370', title: 'Quiz 1', dueDate: '2024-7-8', score: 89},
+        // {courseId: 'cst438', title: 'Assignment 1', dueDate: '2024-7-8', score: 79},
+        // {courseId: 'cst438', title: 'Quiz 1', dueDate: '2024-7-8', score: 84}
+    ])
+
+    const [message, setMessage] = useState('');
+    const [search, setSearch] = useState({studentId, year: '', semester: ''});
+
+    const fetchAssignments = async () => {
+        let semesters = ['Spring', 'Fall', 'Summer', 'Winter'];
+        setMessage('');
+        if (search.studentId === '' || search.year === '' || search.semester === '') {
+            setMessage("Enter search");
+        } else if (!semesters.includes(search.semester)) {
+            setMessage("Semester must be Spring, Fall, Summer, or Winter");
+        } else if (/^\d+$/.test(search.year) === false) {
+            setMessage("Year must be a number");
+        } else {
+            try {
+                const response = await fetch(`${SERVER_URL}/assignments?studentId=${search.studentId}&year=${search.year}&semester=${search.semester}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setAssignment(data);
+                } else {
+                    const r = await response.json();
+                    setMessage(r.message);
+                }
+            } catch(err){
+                setMessage("Error: " +err);
+            }
+        }
+    }
+
+    const editChange = (event) => {
+        setSearch({...search,  [event.target.name]:event.target.value});
+    }
      
     return(
-        <> 
-            <h3>Not implemented</h3>   
+        <>
+            <div>
+                <h3>Assignments</h3>
+
+                <h4>{message}</h4>
+                <h4>Enter year and semester for Assignments you wish to retrieve:</h4>
+
+                <table className="Center">
+                    <tbody>
+                    <tr>
+                        <td>Year:</td>
+                        <td><input type="text" id="year_id" name="year" value={search.year} onChange={editChange} /></td>
+                    </tr>
+                    <tr>
+                        <td>Semester:</td>
+                        <td><input type="text" id="semester_id" name="semester" value={search.semester} onChange={editChange} /></td>
+                    </tr>
+                    </tbody>
+                </table>
+                <br/>
+                <button type="submit" id="search" onClick={fetchAssignments}>Search for Assignments</button>
+                <br/>
+                <br/>
+                <table className="Center">
+                    <thead>
+                        <tr>
+                            {headers.map((h,idx) => <th key={idx}>{h}</th>)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {assignments.map((assignment) =>
+                            <tr key={assignment.courseId}>
+                                <td>{assignment.courseId}</td>
+                                <td>{assignment.title}</td>
+                                <td>{assignment.dueDate}</td>
+                                <td>{assignment.score}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
--- src/components/student/ScheduleView.js
+++ src/components/student/ScheduleView.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {SERVER_URL} from '../../Constants';
+import { confirmAlert } from 'react-confirm-alert';
+import Button from '@mui/material/Button';
 
 // student can view schedule of sections 
 // use the URL /enrollment?studentId=3&year= &semester=
@@ -10,10 +13,135 @@ import React from 'react';
 
 const ScheduleView = (props) => {
     
+    const headers = ['EnrollmentId', 'CourseId', 'SecId',  'Year', 'Semester', 'Building', 'Room', 'Times', '', ''];
+
+    const [search, setSearch] = useState({
+        year: '',
+        semester: ''
+    });
+    const [message, setMessage] = useState('');
+    const [schedules, setSchedules] = useState([
+        // {enrollmentId: "1", courseId: "cst338", sectionId: "1", year: "2024", semester: "Fall", building: "Science", room: "110", times: "11:00am-1:00pm"},
+        // {enrollmentId: "1", courseId: "cst338", sectionId: "1", year: "2024", semester: "Fall", building: "Science", room: "110", times: "11:00am-1:00pm"},
+        // {enrollmentId: "1", courseId: "cst338", sectionId: "1", year: "2024", semester: "Summer", building: "Science", room: "110", times: "11:00am-1:00pm"},
+        // {enrollmentId: "1", courseId: "cst338", sectionId: "1", year: "2024", semester: "Summer", building: "Science", room: "110", times: "11:00am-1:00pm"}
+    ]);
+
+    const fetchSections = async () => {
+            let semesters = ['Spring', 'Fall', 'Summer', 'Winter'];
+            setMessage('');
+            if (search.year === '' || search.semester === '') {
+              setMessage("Search schedule");
+            } else if (!semesters.includes(search.semester)) {
+              setMessage("Must be Spring, Fall, Summer, or Winter");
+            } else if (/^\d+$/.test(search.year) === false) {
+              setMessage("Invalid Year");
+            } else {
+              try {
+                const response = await fetch(`${SERVER_URL}/enrollments?studentId=3&year=${search.year}&semester=${search.semester}`);
+                console.log('Fetching data from URL:', `${SERVER_URL}/enrollments?studentId=3&year=${search.year}&semester=${search.semester}`);
+                console.log('Fetch response status:', response.status);
+
+                if (response.ok) {
+                  const data = await response.json();
+                  setSchedules(data);
+                } else {
+                  const r = await response.json();
+                  setMessage(r.message);
+                }
+              } catch(err) {
+                setMessage("Error: " + err);
+              }
+            }
+        }
+
+        const deleteSchedule = async (enrollmentId) => {
+            try {
+              const response = await fetch (`${SERVER_URL}/enrollments/${enrollmentId}`,
+              {
+                method: 'DELETE',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              });
+              if (response.ok) {
+                setMessage("Schedule deleted");
+                fetchSections();
+              } else {
+                const r = await response.json();
+                setMessage(r.message);
+              }
+            } catch (err) {
+              setMessage("Error: " + err);
+            }
+          }
+
+          const editChange = (event) => {
+            setSearch({...search,  [event.target.name]:event.target.value});
+        }
+
+        const onDelete = (SecNo) => {
+          confirmAlert({
+              title: 'Confirm!',
+              message: 'Delete?',
+              buttons: [
+                {
+                  label: 'Yes',
+                  onClick: () => deleteSchedule(SecNo)
+                },
+                {
+                  label: 'No',
+                }
+              ]
+            });
+        }
    
     return(
         < > 
-            <h3>Not implemented</h3>
+              <div>
+                  <h3>Schedule</h3>
+
+                  <h4>{message}</h4>
+                  <h4>Enter year, semester.  Ex. Year/Semester(2024/Fall)</h4>
+                  <table className="Center">
+                      <tbody>
+                        <tr>
+                          <td>Year:</td>
+                          <td><input type="text" id="year_id" name="year" value={search.year} onChange={editChange} /></td>
+                        </tr>
+                        <tr>
+                          <td>Semester:</td>
+                          <td><input type="text" id="semester_id" name="semester" value={search.semester} onChange={editChange} /></td>
+                        </tr>
+                      </tbody>
+                  </table>
+                  <br/>
+                  <button type="submit" id="search" onClick={fetchSections}>Search for Schedule(s)</button>
+                  <br/>
+                  <br/>
+                  <table className="Center" >
+                      <thead>
+                      <tr>
+                          {headers.map((schedule, idx) => (<th key={idx}>{schedule}</th>))}
+                      </tr>
+                      </thead>
+                      <tbody>
+                      {schedules.map((schedule) => (
+                              <tr key={schedule.enrollmentId}>
+                              <td>{schedule.enrollmentId}</td>
+                              <td>{schedule.courseId}</td>
+                              <td>{schedule.sectionId}</td>
+                              <td>{schedule.year}</td>
+                              <td>{schedule.semester}</td>
+                              <td>{schedule.building}</td>
+                              <td>{schedule.room}</td>
+                              <td>{schedule.times}</td>
+                              <td><Button onClick={() => onDelete(schedule.enrollmentId)}> Drop Class</Button></td>
+                              </tr>
+                          ))}
+                      </tbody>
+                  </table>
+              </div>
         </ >
     );
 
