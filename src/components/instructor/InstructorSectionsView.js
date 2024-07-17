f01e1cf665d7c80f5e940d5c1b52d8bf7052acb2
Michael Cervantes <indiedevmichael@gmail.com>
2024-07-16 22:06:17 -0700

    Added ID to Enrollments\Assignments LINK tags in InstructorSectionsView.js

--- src/components/instructor/InstructorSectionsView.js
+++ src/components/instructor/InstructorSectionsView.js
@@ -60,8 +60,8 @@ const InstructorSectionsView = (props) => {
                                 <td>{s.building}</td>
                                 <td>{s.room}</td>
                                 <td>{s.times}</td>
-                                <td><Link to="/enrollments" state={s}>Enrollments</Link></td>
-                                <td><Link to="/assignments" state={s}>Assignments</Link></td>
+                                <td><Link id="enroll" to="/enrollments" state={s}>Enrollments</Link></td>
+                                <td><Link id="assignment" to="/assignments" state={s}>Assignments</Link></td>
                                 </tr>
                             ))}
                         </tbody>
